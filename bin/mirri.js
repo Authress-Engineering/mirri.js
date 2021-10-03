ddba90804a2ac5fb19de3097961191c99f7fcb1b
Warren Parad <wparad@gmail.com>
2021-10-03 22:44:13 +0200

    Add some lint.

--- bin/mirri.js
+++ bin/mirri.js
@@ -1,54 +1,54 @@
 #!/usr/bin/env node
 
-var commander = require('commander');
-var path = require('path');
-var Mirri = require('../index');
-var aws = require('aws-sdk');
+let commander = require('commander');
+let path = require('path');
+let Mirri = require('../index');
+let aws = require('aws-sdk');
 
-var version = require(path.join(__dirname, '../package.json')).version;
+let version = require(path.join(__dirname, '../package.json')).version;
 commander.version(version);
 
 commander
-	.command('rotate [profile]')
-	.usage('rotate [options] [profile]')
-	.description('Rotate AWS key, defaults to default')
-	.action(async (profile, options) => {
-		var selectedProfile = profile || 'default';
-		aws.config.credentials = new aws.SharedIniFileCredentials({profile: selectedProfile});
-		var mirri = new Mirri();
-		await mirri.Rotate(selectedProfile, options.force)
-		console.log('AWS key rotation Complete.');
-	});
+.command('rotate [profile]')
+.usage('rotate [profile]')
+.description('Rotate AWS key, defaults to default')
+.action(async profile => {
+  let selectedProfile = profile || 'default';
+  aws.config.credentials = new aws.SharedIniFileCredentials({ profile: selectedProfile });
+  let mirri = new Mirri();
+  await mirri.Rotate(selectedProfile);
+  console.log('AWS key rotation Complete.');
+});
 commander
-	.command('cleanup [profile]')
-	.usage('cleanup [profile]')
-	.description('Removes unused key from IAM user.')
-	.action(async (profile) => {
-		var selectedProfile = profile || 'default';
-		aws.config.credentials = new aws.SharedIniFileCredentials({profile: selectedProfile});
-		var mirri = new Mirri();
-		await mirri.Cleanup(selectedProfile)
-		console.log('Cleanup Complete.');
-	});
+.command('cleanup [profile]')
+.usage('cleanup [profile]')
+.description('Removes unused key from IAM user.')
+.action(async profile => {
+  let selectedProfile = profile || 'default';
+  aws.config.credentials = new aws.SharedIniFileCredentials({ profile: selectedProfile });
+  let mirri = new Mirri();
+  await mirri.Cleanup(selectedProfile);
+  console.log('Cleanup Complete.');
+});
 commander
-	.command('schedule [profile] [frequency]')
-	.usage('schedule [options] [profile] [frequency]')
-	.option('-p, --path', 'Register with the full path of mirri.', false)
-	.description('Schedule a crontab task to rotate keys.')
-	.action(async (profile, frequency, options) => {
-		var selectedProfile = profile || 'default';
-		var specifiedFrequency = frequency || '@weekly';
-		aws.config.credentials = new aws.SharedIniFileCredentials({profile: selectedProfile});
-		var mirri = new Mirri();
-		await mirri.Schedule(selectedProfile, specifiedFrequency, options.path);
-		console.log('Mirri scheduled.');
-	});
+.command('schedule [profile] [frequency]')
+.usage('schedule [options] [profile] [frequency]')
+.option('-p, --path', 'Register with the full path of mirri.', false)
+.description('Schedule a crontab task to rotate keys.')
+.action(async (profile, frequency, options) => {
+  let selectedProfile = profile || 'default';
+  let specifiedFrequency = frequency || '@weekly';
+  aws.config.credentials = new aws.SharedIniFileCredentials({ profile: selectedProfile });
+  let mirri = new Mirri();
+  await mirri.Schedule(selectedProfile, specifiedFrequency, options.path);
+  console.log('Mirri scheduled.');
+});
 
 commander.on('*', () => {
-	if(commander.args.join(' ') == 'tests/**/*.js') { return; }
-	console.log('Unknown Command: ' + commander.args.join(' '));
-	commander.help();
-	process.exit(0);
+  if (commander.args.join(' ') == 'tests/**/*.js') { return; }
+  console.log(`Unknown Command: ${commander.args.join(' ')}`);
+  commander.help();
+  process.exit(0);
 });
 
 commander.parse(process.argv[2] ? process.argv : process.argv.concat(['help']));
--- make.js
+++ make.js
@@ -3,46 +3,46 @@
 /**
  * Module dependencies
  */
-var commander = require('commander');
-var fs = require('fs');
+let commander = require('commander');
+let fs = require('fs-extra');
 
-var ci = require('ci-build-tools')(process.env.GIT_TAG_PUSHER);
-var version = ci.GetVersion();
+let ci = require('ci-build-tools')(process.env.GIT_TAG_PUSHER);
+let version = ci.GetVersion();
 commander.version(version);
 
 /**
  * Build
  */
 commander
-	.command('build')
-	.description('Setup require build files for npm package.')
-	.action(() => {
-		var package_metadata = require('./package.json');
-		package_metadata.version = version;
-		fs.writeFileSync('./package.json', JSON.stringify(package_metadata, null, 2));
+.command('build')
+.description('Setup require build files for npm package.')
+.action(async () => {
+  let package_metadata = require('./package.json');
+  package_metadata.version = version;
+  await fs.writeFile('./package.json', JSON.stringify(package_metadata, null, 2));
 
-		console.log("Building package %s (%s)", package_metadata.name, version);
-		console.log('');
-	});
+  console.log('Building package %s (%s)', package_metadata.name, version);
+  console.log('');
+});
 
 /**
  * After Build
  */
 commander
-	.command('after_build')
-	.description('Publishes git tags and reports failures.')
-	.action(() => {
-		var package_metadata = require('./package.json');
-		console.log("After build package %s (%s)", package_metadata.name, version);
-		console.log('');
-		ci.PublishGitTag();
-		ci.MergeDownstream('release/', 'master');
-	});
+.command('after_build')
+.description('Publishes git tags and reports failures.')
+.action(() => {
+  let package_metadata = require('./package.json');
+  console.log('After build package %s (%s)', package_metadata.name, version);
+  console.log('');
+  ci.PublishGitTag();
+  ci.MergeDownstream('release/', 'master');
+});
 
 commander.on('*', () => {
-	if(commander.args.join(' ') == 'tests/**/*.js') { return; }
-	console.log('Unknown Command: ' + commander.args.join(' '));
-	commander.help();
-	process.exit(0);
+  if (commander.args.join(' ') === 'tests/**/*.js') { return; }
+  console.log(`Unknown Command: ${commander.args.join(' ')}`);
+  commander.help();
+  process.exit(0);
 });
 commander.parse(process.argv[2] ? process.argv : process.argv.concat(['build']));
